0771902ea78bd6cbbda8fbfbf1590caeeb732168
Morenogoldjr <leonardo.ivan.morenog@gmail.com>
2026-05-15 12:41:52 -0600

    Regreso a normallidad

--- Backend/webPOO/src/main/java/com/negocio/webPOO/model/Usuario.java
+++ Backend/webPOO/src/main/java/com/negocio/webPOO/model/Usuario.java
@@ -9,7 +9,6 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre;
     private String correo;
     private String password;
 
@@ -19,14 +18,6 @@ public class Usuario {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getCorreo() {
--- Backend/webPOO/src/main/java/com/negocio/webPOO/repository/UsuarioRepository.java
+++ Backend/webPOO/src/main/java/com/negocio/webPOO/repository/UsuarioRepository.java
@@ -2,8 +2,6 @@ package com.negocio.webPOO.repository;
 
 import com.negocio.webPOO.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByCorreo(String correo);
 }
